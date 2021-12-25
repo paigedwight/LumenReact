b6c361182bf48d9306eee21f2cc36a94ce38779f
Paige Dwight <paigedwight@users.noreply.github.com>
2021-12-24 23:35:27 -0500

    remove dev build stages

--- server/composer.sh
+++ server/composer.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v "$(cd "$(dirname "$0")"; pwd)":/app  composer:2.1.14 composer install
+docker run -it --rm -v "$(cd "$(dirname "$0")"; pwd)":/app composer:2.1.14 composer "$@"
