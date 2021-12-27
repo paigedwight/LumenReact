8b8c4667e4b88b234a7f3e5f061f48216b831f9e
Paige Dwight <paigedwight@users.noreply.github.com>
2021-12-27 18:10:59 -0500

    restructure dockerfiles, run package installs manually

---
+++ server/devscripts/composer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ex
+
+declare repoDir="$(git rev-parse --show-toplevel)"
+declare image="$(docker build --target dev -q "${repoDir}/server")"
+
+if [ "$1" == "install" ]; then
+    set -- install --ignore-platform-reqs --no-interaction --no-scripts "${@:2}"
+fi
+
+docker run --rm -it --workdir /var/www -v "${repoDir}/server:/var/www" "$image" composer "$@"
--- web/.docker/docker-nextjs-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -ex
-
-npm install
-
-exec npm run dev "$@"
