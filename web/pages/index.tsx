74f09baa9e60977f6a2782a488c189f972757ffe
Paige Dwight <paigedwight@users.noreply.github.com>
2021-12-27 18:37:08 -0500

    configure and run prettier

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,26 +1,26 @@
-import type { NextPage } from 'next'
+/** @format */
+
+import type { NextPage } from 'next';
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
 interface Props {
-  content?: string;
+    content?: string;
 }
 
 const Home: NextPage<Props> = ({ content }) => {
-    return (
-        <div>{content}</div>
-    );
-}
+    return <div>{content}</div>;
+};
 
 export const getServerSideProps = async () => {
-  const version = await fetch(`http://nginx/api/`)
-      .then(response => response.json())
-      .then(data => data.version);
+    const version = await fetch(`http://nginx/api/`)
+        .then(response => response.json())
+        .then(data => data.version);
 
-  return {
-    props: { content: version },
-  }
+    return {
+        props: { content: version },
+    };
 };
 
 export default Home;
